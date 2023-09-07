b3c88bd938da31585f938b01c5a101a3bde962cf
MarselGaisin <marselgaisin@gmail.com>
2023-09-07 11:50:02 +0300

    test git

--- message-service/src/main/java/com/chatus/messageservicemodule/MessageServiceModuleApplication.java
+++ message-service/src/main/java/com/chatus/messageservicemodule/MessageServiceModuleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessageServiceModuleApplication {
-//
+
     public static void main(String[] args) {
         SpringApplication.run(MessageServiceModuleApplication.class, args);
     }
